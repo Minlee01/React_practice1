9aab9e13b9055ec79efb6de6a737319d1fc46adb
Minkyung Lee <minlee4321@gmail.com>
2023-10-23 23:59:23 +0900

    ch08-Event handle-classcomponent-bind

---
+++ src/chapter_08/ConfirmButton.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+class ConfirmButton extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state ={ //화인여부 저장
+            isConfirmed: false,
+        };
+
+        this.handleConfirm = this.handleConfirm.bind(this);
+    }
+
+    handleConfirm() { //onClick event 처리
+        this.setState((prevState) => ({
+            isConfirmed: !prevState.isConfirmed,
+        }));
+    }
+
+    render() {
+        return (
+            <button 
+                onClick={this.handleConfirm}
+                disabled={this.state.isConfirmed}
+                >
+                    {this.state.isConfirmed ? "확인됨" : "확인하기"}
+                </button>
+        );
+    }
+}
+
+export default ConfirmButton;
